e046c27a0a86c495dedb3d449834460b936a275c
Valentina Danilova <danilova.vv@phystech.edu>
2024-01-28 17:13:04 +0400

    feat: style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,29 @@
 import './styles.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Header() {
   return (
     <div className='header'>
-      <Link href='/' className='header__name'>
+      <Link href='/' className='logo header__logo'>
         Comfort Store
       </Link>
       <div className='header__links'>
-        <Link href='/'>Shop</Link>
-        <Link href='/about'>About</Link>
+        <Link className='link' href='/'>
+          Shop
+        </Link>
+        <Link className='link' href='/about'>
+          About
+        </Link>
       </div>
-      <Link href='/cart' className='header__cart'>
-        Cart
+      <Link href='/cart' className='cart header__cart'>
+        <Image
+          src='/assets/icons/shopping-cart.svg'
+          alt='shopping cart'
+          width={24}
+          height={24}
+          className='cart__icon'
+        />
       </Link>
     </div>
   );

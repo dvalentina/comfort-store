b5ba2783e2f30911aa6715656042c5493b867968
Valentina Danilova <danilova.vv@phystech.edu>
2024-02-02 02:58:47 +0400

    fix: fix storybook env variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.STORYBOOK
-  ? process.env.NEXT_PUBLIC_STORYBOOK_URL
+  ? process.env.STORYBOOK_BASE_URL
   : process.env.NEXT_PUBLIC_BASE_URL;
 
 export const API_URL = `${BASE_URL}/api`;

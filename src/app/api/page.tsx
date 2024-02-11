6c13e8452499c3342bf0304eb209d0caac8f1bc7
Valentina Danilova <danilova.vv@phystech.edu>
2024-02-12 03:14:53 +0400

    feat: add styles to api entry page

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,17 +1,37 @@
+import './styles.scss';
+
+import Typography from '@/components/Typography';
+
 function APIHome() {
   return (
-    <>
-      <h1>Comfort Store API</h1>
-      <h3>Available routes:</h3>
-      <ul>
-        <li>
-          <a href='api/products'>/api/products</a>
-        </li>
-        <li>
-          <a href='api/products/1'>/api/products/:id</a>
-        </li>
-      </ul>
-    </>
+    <div className='api-page'>
+      <Typography role='h1' type='headline' size='large'>
+        Comfort Store API
+      </Typography>
+      <div>
+        <Typography role='h3' type='title' size='medium'>
+          Available routes:
+        </Typography>
+        <ul>
+          <li>
+            <Typography type='label' role='span'>
+              GET{' '}
+            </Typography>
+            <Typography size='large' role='span'>
+              <a href='api/products'>/api/products</a>
+            </Typography>
+          </li>
+          <li>
+            <Typography type='label' role='span'>
+              GET{' '}
+            </Typography>
+            <Typography size='large' role='span'>
+              <a href='api/products/1'>/api/products/:id</a>
+            </Typography>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
